feb325353127df49d35f580be8de2e14f6d2e8ca
Meybili <109104859+meybili19@users.noreply.github.com>
2025-02-12 01:24:32 -0500

    er

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,20 +1,36 @@
-from flask import jsonify
+import os
+import requests
+from flask import request, jsonify
 from src.models.models import db, User
+from dotenv import load_dotenv
 
-# Route to delete user by ID
+# Load environment variables from .env
+load_dotenv()
+
+# Get the microservice URL from environment variables
+QUERY_MICROSERVICE_URL = os.getenv('QUERY_MICROSERVICE_URL')
+
+# Route to delete user
 def delete_user(id):
     try:
-    # Search for the user by its ID
-        user = User.query.get(id)
-
+        # Make a request to the microservice to get the user by their ID
+        response = requests.get(f'{QUERY_MICROSERVICE_URL}/{id}')
+        
         # Check if the user exists
-        if not user:
+        if response.status_code != 200:
             return jsonify({'error': 'User not found'}), 404
+
+        user = User.query.get(id)
+        
+        # Check if the user exists in the database
+        if not user:
+            return jsonify({'error': 'User not found in local database'}), 404
 
         # Delete the user
         db.session.delete(user)
         db.session.commit()
-        return jsonify({'message': 'User deleted successfully OK'}), 200
+        
+        return jsonify({'message': 'User deleted successfully'}), 200
     
     except Exception as e:
         return jsonify({'error': str(e)}), 400
